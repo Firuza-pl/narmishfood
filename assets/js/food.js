77a1aa294abac878001f3913b73d7089a6a06d31
Firuza <rustemlifiruze@gmail.com>
2026-01-05 13:48:32 +0400

    test pwa

--- assets/js/food.js
+++ assets/js/food.js
@@ -1,10 +1,33 @@
-  AOS.init({
-    duration: 600, // animation duration in ms
-    once: true
-  });
+AOS.init({
+  duration: 600, // animation duration in ms
+  once: true
+});
 
-  // Refresh AOS every time the mobile menu opens
-  const navbarCollapse = document.getElementById('navbarResponsive');
-  navbarCollapse.addEventListener('shown.bs.collapse', () => {
-    AOS.refresh(); // triggers animation when menu becomes visible
-  });
+// Refresh AOS every time the mobile menu opens
+const navbarCollapse = document.getElementById('navbarResponsive');
+navbarCollapse.addEventListener('shown.bs.collapse', () => {
+  AOS.refresh(); // triggers animation when menu becomes visible
+});
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('../../sw.js')
+    .then(() => console.log('Service Worker registered'))
+    .catch(err => console.log(err));
+}
+
+let deferredPrompt;
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  console.log('PWA can be installed!');
+});
+
+function showInstallPrompt() {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then(choiceResult => {
+      console.log(choiceResult.outcome);
+      deferredPrompt = null;
+    });
+  }
+}
